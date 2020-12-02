eed050d2a544ef8abd5909f65b428e3a9f2d53c3
Tommaso Goni <44438981+TommyT0mmY@users.noreply.github.com>
2020-12-02 16:27:31 +0100

    Correzione input da tastiera

--- main.cpp
+++ main.cpp
@@ -39,22 +39,30 @@ int main()
     for(int x = 0; x < ConsoleWidth; ++x)
         for(int y = 0; y < ConsoleHeight; ++y)
             screen[y * ConsoleWidth + x] = ' ';
+    freopen("logs.txt","w",stdout); //debug
+    //handle per l'input
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+
 
     while(1)
     {
         Sleep(1);
-        //lettura del'input
-        INPUT_RECORD inputRecord;
-        DWORD cNumRead;
-        ReadConsoleInput(hConsole, &inputRecord, 1, &cNumRead);
-        for (int i = 0; i < cNumRead; ++i)
-        {
-            if(inputRecord.EventType == KEY_EVENT && ((KEY_EVENT_RECORD&)inputRecord.Event).bKeyDown)
+        //lettura degli input
+        DWORD numberOfInputs;
+        GetNumberOfConsoleInputEvents(hInput, &numberOfInputs);
+        if (numberOfInputs > 0){
+            INPUT_RECORD irInBuf[128];
+
+            ReadConsoleInput(hInput, irInBuf, 128, &numberOfInputs);
+            for (int i = 0; i < numberOfInputs; ++i)
             {
-                WriteToScreen(screen, 10, 11, 'a'); //TODO RISOLVERE PROBLEMA CON L'INPUT
+                if (irInBuf[i].EventType == KEY_EVENT && irInBuf[i].Event.KeyEvent.bKeyDown)
+                {
+                    screen[10*ConsoleWidth+10]=irInBuf[i].Event.KeyEvent.uChar.AsciiChar;
+                }
             }
         }
-
+        
 
         //scrittura buffer schermo
         screen[ConsoleWidth * ConsoleHeight - 1] = '\0';
